104a8e00fb519fc711b9515ebc42c2c1cd34bc76
MohsinGhori <MohsinAKG@Gmail.com>
2017-01-05 10:19:39 +0500

    Add files via upload
    Assalam Aly Kum is the greetings in URDU and ARABIC like Hello in ENGLISH.

---
+++ Assalam_Aly_Kum.java
@@ -0,0 +1,9 @@
+/* Assalam Aly Kum (Hello) printing program */
+
+public class Assalam_Aly_Kum
+{
+public static void main(String args[])
+{
+System.out.print("Assalam-Aly-Kum (Hello)! WORLD");
+}	// eof main method.
+}   // eof class Assalam_Aly_Kum
